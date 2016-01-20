3e9faf3faf2ba64754f8f51cb2a613a7961b0e5b
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-01-20 12:45:42 +0100

    simple eval test

--- test/sw-eval-test.js
+++ test/sw-eval-test.js
@@ -5,14 +5,8 @@ import * as $ from 'https://code.jquery.com/jquery-2.1.4.js';
 var expect = chai.expect;
 
 describe('Eval', function() {
-  it('should eval 3 + 4 to 7', function(done) {
-    $.ajax({
-      url: "https://eval/3+4",
-      type: "get",
-      success: function(actualResult) {
-        expect(actualResult).to.equal('7');
-        done();
-      }
-    });
+  it('should eval 3 + 4 to 7', function() {
+    expect(eval('3+4')).to.equal(7);
+    expect($.trim('  thing with whitespaces  ')).to.equal('thing with whitespaces');
   });
 });
