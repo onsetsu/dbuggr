9e565c4164c184791a06851525b54a18daa28821
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-01-21 15:42:41 +0100

    include clusterview

--- dbuggr.js
+++ dbuggr.js
@@ -11,78 +11,8 @@ ServerProxy.browse('./vendor', ServerProxy.types.scripts).then(files => {
     console.log(files);
 });
 
-/*
-var margin = {top: 20, right: 20, bottom: 30, left: 50},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
-
-var formatDate = d3.time.format("%d-%b-%y");
-
-var x = d3.time.scale()
-    .range([0, width]);
-
-var y = d3.scale.linear()
-    .range([height, 0]);
-
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
-
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var line = d3.svg.line()
-    .x(function(d) { return x(d.date); })
-    .y(function(d) { return y(d.close); });
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.tsv("example/data.tsv", type, function(error, data) {
-    if (error) throw error;
-
-    x.domain(d3.extent(data, function(d) { return d.date; }));
-    y.domain(d3.extent(data, function(d) { return d.close; }));
-
-    svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0," + height + ")")
-        .call(xAxis);
-
-    svg.append("g")
-        .attr("class", "y axis")
-        .call(yAxis)
-        .append("text")
-        .attr("transform", "rotate(-90)")
-        .attr("y", 6)
-        .attr("dy", ".71em")
-        .style("text-anchor", "end")
-        .text("Price ($)");
-
-    svg.append("path")
-        .datum(data)
-        .attr("class", "line")
-        .attr("d", line);
-});
-
-function type(d) {
-    d.date = formatDate.parse(d.date);
-    d.close = +d.close;
-    return d;
-}
-*/
-
-/*********************************************************************
- *********************************************************************
- *********************************************************************
- *********************************************************************
- *********************************************************************
- *********************************************************************
- *********************************************************************/
+// Sunburst with changable sizes http://bl.ocks.org/mbostock/4063423
+// TODO: Zoomable Sunburst http://bl.ocks.org/kerryrodden/477c1bfb081b783f80ad
 
 var width = 960,
     height = 700,
@@ -109,7 +39,7 @@ var arc = d3.svg.arc()
     .startAngle(d => d.x)
     .endAngle(d => d.x + d.dx)
     .innerRadius(function(d) {
-        return radius * (!d.children ?
+        return radius * (d.children ?
                 pieInverter(d.y + d.dy) :
                     0.3
             );
