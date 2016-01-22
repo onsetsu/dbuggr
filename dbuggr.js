379fd15c900eb6d20b4081dd21f33b5525d0e7f5
Stefan Lehmann <stefan.lehmann@student.hpi.uni-potsdam.de>
2016-01-22 18:48:02 +0100

    path gradients for bundle paths

--- dbuggr.js
+++ dbuggr.js
@@ -20,14 +20,44 @@ ServerProxy.browse('./vendor', ServerProxy.types.scripts).then(files => {
 //   via transform?
 // TODO: hook with your own data
 // TODO: Folder and file names
-// TODO: On hover: Highlight hierarchy and dependencies
+// On hover: Highlight hierarchy and dependencies
+// path gradients
+// TODO: Reloadable
 
 var width = 960,
     height = 700,
     radius = Math.min(width, height) / 2,
     color = d3.scale.category20c();
+/*
+<defs>
 
-var svg = d3.select("body").append("svg")
+<linearGradient id='grad'>
+    <stop stop-color='black'/>
+    <stop offset='100%' stop-color='magenta'/>
+    </linearGradient>
+
+    <marker id="start-mark" viewBox="-1 -1 2 2"  markerWidth="1.2" markerHeight="1.2" orient='auto'>
+    <circle r='1'/>
+    </marker>
+
+    <marker id="end-mark" viewBox="-1 -1 2 2"  markerWidth="2.5" markerHeight="2.5" orient='auto'>
+    <!-->  <polygon points='0,1 1,0 0,-1'/> <!-- -->
+    <-->  <path class='marker-path' d='M0,1 L1,0 L0,-1 Z'/>  <!-- -->
+    </marker>
+
+    </defs>
+*/
+var realSVG = d3.select("body").append("svg");
+
+// gradient as reference for hierarchical edge bundles
+var gradient = realSVG.append('defs').append('linearGradient')
+    .attr("id", 'bundleview-gradient');
+gradient.append('stop').attr('stop-color', '#2ca02c');
+gradient.append('stop')
+    .attr('stop-color', '#d62728')
+    .attr('offset', '100%');
+
+var svg = realSVG
     .attr("width", width)
     .attr("height", height)
     .append("g")
@@ -90,10 +120,11 @@ d3.json("example/flare.json", function(error, root) {
     var path = enterElem.append("path")
         .attr("display", function(d) { return null; return d.depth ? null : "none"; }) // hide inner ring
         .attr("d", arc)
-        .style("stroke", "#fff")
-        .style("fill", function(d) { return color((d.children ? d : d.parent).name); })
+        //.style("fill", function(d) { return color((d.children ? d : d.parent).name); })
         .style("fill-rule", "evenodd")
         .each(stash)
+        .classed('node', true)
+        .classed('node--leaf', d => !d.children)
         .on('click', (d => console.log(d.name, d)))
         .on("mouseover", mouseovered)
         .on("mouseout", mouseouted);
@@ -136,28 +167,62 @@ d3.json("example/flare.json", function(error, root) {
         .attr("class", "link")
         .attr("d", line);
 
-    function mouseovered(d) { console.log("overed");
+    function mouseovered(d) {
+        function markDescendants(n) {
+            if(n.children) {
+                n.children.forEach(child => {
+                    child.descendant = true;
+                    markDescendants(child);
+                });
+            }
+        }
+
+        function markAncestors(n) {
+            if(n.parent) {
+                markAncestors(n.parent);
+                n.parent.ancestor = true;
+            }
+        }
+
+        // clear node markers
         node
-            .each(function(n) { n.target = n.source = false; });
+            .each(function(n) { n.target = n.source = n.descendant = n.ancestor = false; });
+
+        markDescendants(d);
+        markAncestors(d);
 
         link
             .each(function(d) {d.source = d[0]; d.target = d[d.length - 1]; })
-            .classed("link--target", function(l) { if (l.target === d) return l.source.source = true; })
-            .classed("link--source", function(l) { if (l.source === d) return l.target.target = true; })
+            .classed("link--target", function(l) {
+                if (l.target === d || l.target.descendant) {
+                    return l.source.source = true;
+                }
+            })
+            .classed("link--source", function(l) {
+                if (l.source === d || l.source.descendant) {
+                    return l.target.target = true;
+                }
+            })
             .filter(function(l) { return l.target === d || l.source === d; })
             .each(function() { this.parentNode.appendChild(this); });
 
         node
+            .classed("node--hovered", function(n) { return n === d; })
+            .classed("node--descendant", function(n) { return n.descendant; })
+            .classed("node--ancestor", function(n) { return n.ancestor; })
             .classed("node--target", function(n) { return n.target; })
             .classed("node--source", function(n) { return n.source; });
     }
 
-    function mouseouted(d) { console.log("outed");
+    function mouseouted(d) {
         link
             .classed("link--target", false)
             .classed("link--source", false);
 
         node
+            .classed("node--hovered", false)
+            .classed("node--descendant", false)
+            .classed("node--ancestor", false)
             .classed("node--target", false)
             .classed("node--source", false);
     }
